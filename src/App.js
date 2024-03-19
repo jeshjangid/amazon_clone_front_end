79e51d07553007b1507e6d9c313f9a8927f3df29
jesh <jeshjangid09@gmail.com>
2024-03-19 13:59:06 +0530

    image slider component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import NavbarContainer from "./components/layout/Navbar/NavbarContainer";
+import LandingPage from "./components/layout/LandingPage";
 
 function App() {
   return (
     <div>
-    <NavbarContainer />
+    <LandingPage />
     </div>
   )
 }
