d92fc0fb0ad1976d4faaa40d5b2dce956aa18358
jesh <jeshjangid09@gmail.com>
2024-03-19 15:46:53 +0530

    Home Page Product

---
+++ src/components/HomePageProduct/HomePageProductContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePageProductView from './HomePageProductView';
+
+const HomePageProductContainer = () => {
+  return (
+    <React.Fragment>
+        <HomePageProductView />
+    </React.Fragment>
+  )
+}
+
+export default HomePageProductContainer;
